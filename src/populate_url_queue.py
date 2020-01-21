6fbf3f502b0062071e5cf4440a534eb19552e285
Andrew Cassin <acassin999@gmail.com>
2020-01-22 08:42:18 +1100

    fix bugs with seed url population

---
+++ src/populate_url_queue.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+from kafka import KafkaProducer
+import csv
+import json
+
+producer = KafkaProducer(bootstrap_servers='kafka1', 
+                         value_serializer=lambda m: json.dumps(m).encode('utf-8'))
+
+with open('/home/acas/data/filtered_au_websites.csv', 'r') as csv:
+     for line in csv:
+        url = "https://" + line
+        future = producer.send('url_queue', {'url': url}) # NB: async!
+
+# ensure all pending sends are done
+producer.flush()
+
+# obtain metrics on delivery
+print(producer.metrics())
