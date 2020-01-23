93d839b76db20312e4d9e38db3f99d45794086ce
Andrew Cassin <acassin999@gmail.com>
2020-01-23 16:11:03 +1100

    ensure all thug hosts have several pip packages required

--- src/populate_url_queue.py
+++ src/populate_url_queue.py
@@ -3,17 +3,23 @@
 from kafka import KafkaProducer
 import csv
 import json
+import uuid
 
 producer = KafkaProducer(bootstrap_servers='kafka1', 
                          value_serializer=lambda m: json.dumps(m).encode('utf-8'))
 
 with open('/home/acas/data/filtered_au_websites.csv', 'r') as csv:
+     done = 0
      for line in csv:
         url = "https://" + line.rstrip()
-        future = producer.send('url_queue', {'url': url}) # NB: async!
+        uuid_bytes = uuid.uuid1().bytes
+        future = producer.send('url_queue', key=uuid_bytes, value={'url': url}) # NB: async!
+        done = done + 1
+        if done % 1000 == 0:
+            print("Processed {} URLs.".format(done))
 
 # ensure all pending sends are done
 producer.flush()
 
 # obtain metrics on delivery
-print(json.dumps(producer.metrics(), sort_keys=True, indent=4)
+print(json.dumps(producer.metrics(), sort_keys=True, indent=4))
