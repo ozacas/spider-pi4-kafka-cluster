35d08e8f1da6761743da3ca1447ba81eefe69842
Andrew Cassin <acassin999@gmail.com>
2020-02-07 17:33:08 +1100

    penalise common media, but dont prevent fetching in case someone is playing games with file extensions

--- src/utils/url.py
+++ src/utils/url.py
@@ -1,6 +1,6 @@
 from urllib.parse import urlparse
 
-def as_priority(url, up=None):
+def as_priority(url, up):
    priority = 1
    penalty = 0
    if up is None:
@@ -9,13 +9,13 @@ def as_priority(url, up=None):
        penalty = up.path.count('/')  # penalty of up to three depending on depth of page
    if penalty > 3:
        penalty = 3
+   if up.path.lower().endswith((".mp3", ".avi", ".pdf", ".png", ".vob", ".mpeg", ".mpg", ".mp4")):
+       penalty = penalty + 2
    qlen = len(up.query)
    if qlen > 0 and qlen < 20:
       penalty = penalty + 1
    elif qlen > 0:
       penalty = penalty + 2
-   if 'anu.edu.au' in up.hostname:
-      penalty = penalty + 5
    if len(url) > 200:
       penalty = penalty + 2
    elif len(url) > 100:
