f1c1ef38d692a3121bf615d4f0c2eacefe07e0b9
Andrew Cassin <acassin999@gmail.com>
2020-02-28 13:54:27 +1100

    Dont override item_completed() as the callbacks are cleaner way to handle persistence

--- src/utils/mypipeline.py
+++ src/utils/mypipeline.py
@@ -1,3 +1,4 @@
+from scrapy import Request
 from scrapy.pipelines.files import FilesPipeline
 from kafka import KafkaProducer
 import json
@@ -6,18 +7,21 @@ from datetime import datetime
 
 class MyFilesPipeline(FilesPipeline):
    producer = KafkaProducer(value_serializer=lambda m: json.dumps(m).encode('utf-8'), bootstrap_servers='kafka1')
+ 
+   def get_media_requests(self, item, info):
+      for url in item['file_urls']:
+          yield Request(url, callback=self.done, errback=self.errback, dont_filter=True, cb_kwargs={'origin_url': item['origin'] })
 
-   def item_completed(self, results, item, info):
-      item = super().item_completed(results, item, info)
-      # submit completion to kafka queue (only if item successfully saved)
-      t = results[0]
-      if t[0]:
-          d = t[1] 
-          d.update({ 'host': socket.gethostname(), 'when': str(datetime.utcnow()), 'origin': item.get('origin', None) })
-          self.producer.send('javascript-artefacts', d)
-      else:
-          # failed to download JS - indicator of malicious-ness???? so we need to send it to kafka
-          d = dict(item)
-          d.update({ 'when': str(datetime.utcnow()) })
-          self.producer.send('javascript-download-failure', d)
-      return item
+   def done(self, response, origin_url=None):
+      response.update({ 'host': socket.gethostname(), 
+                        'when': str(datetime.utcnow()), 
+                        'origin': origin_url })
+      self.producer.send('javascript-artefacts2', d)
+      return response
+
+   def errback(self, failure, origin_url=None, item=None):
+      d = { 'origin': origin_url,
+            'when': str(datetime.utcnow()),
+            'url': failure.request.url,
+            'host': socket.gethostname() }
+      self.producer.send('javascript-download-failure', d)
