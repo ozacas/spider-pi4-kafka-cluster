3e2a991debb845236d3a9c3f0f98068ed3701363
Andrew Cassin <acassin999@gmail.com>
2020-03-08 19:59:08 +1100

    Use python dataclasses to provide topic record models-as-truth

---
+++ src/utils/models.py
@@ -0,0 +1,23 @@
+from dataclasses import dataclass
+
+@dataclass
+class PageStats:
+   url: str
+   when: str
+   scripts: str = '' # whitespace separated list of <script src=X> URLs
+   n_hrefs: int = 0
+   n_hrefs_max_permitted: int = 0
+   n_external: int = 0
+   n_external_accepted: int = 0
+   n_internal: int = 0
+   n_internal_accepted: int = 0
+   n_scripts: int = 0
+   n_scripts_accepted: int = 0
+
+@dataclass
+class DownloadFailure:
+   reason: str
+   url: str    # valid URL
+   origin: str # valid URL
+   http_status: int # eg. 404 
+   when: str # UTC timestamp
