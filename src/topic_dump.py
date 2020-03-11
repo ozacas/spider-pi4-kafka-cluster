88d6509486cc13df308229a7ebfc9513a3ef99af
Andrew Cassin <acassin999@gmail.com>
2020-03-11 14:41:10 +1100

    Update argument doco

--- src/topic_dump.py
+++ src/topic_dump.py
@@ -4,7 +4,7 @@ import json
 import argparse
 
 a = argparse.ArgumentParser(description="Read messages from the specified topic and dump to stdout in one-record-per-line JSON format")
-a.add_argument("--topic", help="Kafka topic to get visited JS summary", type=str, required=True)
+a.add_argument("--topic", help="Kafka topic to read", type=str, required=True)
 a.add_argument("--bootstrap", help="Kafka bootstrap servers [kafka1]", type=str, default="kafka1")
 a.add_argument("--n", help="Read no more than N records from kafka [infinite]", type=int, default=1000000000)
 a.add_argument("--group", help="Use specified kafka consumer group to remember where we left off [None]", type=str, default=None)
@@ -20,7 +20,7 @@ if args.group is None:
 else:
     position = 'latest'
 consumer = KafkaConsumer(args.topic, group_id=args.group, value_deserializer=lambda m: json.loads(m.decode('utf-8')), 
-                         key_deserializer=lambda m: m.decode('utf-8'), auto_offset_reset=position,
+                         key_deserializer=lambda m: m.decode('utf-8') if m else None, auto_offset_reset=position,
                          bootstrap_servers=args.bootstrap, consumer_timeout_ms=10000)
 
 cnt = 0
