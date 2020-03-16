2b7af39cec809072632ad2e68f1d623f5dbbe6f0
Andrew Cassin <acassin999@gmail.com>
2020-03-16 17:13:45 +1100

    Add argparse support to parameterise execution

--- src/features_to_mongo.py
+++ src/features_to_mongo.py
@@ -3,11 +3,29 @@ import argparse
 import json
 from kafka import KafkaConsumer
 import pymongo 
+import argparse
 
-consumer = KafkaConsumer('analysis-results', group_id='features2mongo', auto_offset_reset='earliest', consumer_timeout_ms=10000, 
-                         bootstrap_servers='kafka1', value_deserializer=lambda m: json.loads(m.decode('utf-8')))
-mongo = pymongo.MongoClient('pi1', 27017)
-db = mongo['au_js']
+a = argparse.ArgumentParser(description="Report scripts from html-page-stats which are not US/AU geolocated based on any IP associated with the URL hostname")
+a.add_argument("--bootstrap", help="Kafka bootstrap servers", type=str, default="kafka1")
+a.add_argument("--n", help="Read no more than N records from kafka [Inf]", type=int, default=1000000000)
+a.add_argument("--topic", help="Read analysis results from specified topic [analysis-results]", type=str, default="analysis-results") # NB: can only be this topic
+a.add_argument("--group", help="Use specified kafka consumer group to remember where we left off [features2mongo]", type=str, default='features2mongo')
+a.add_argument("--v", help="Debug verbosely", action="store_true")
+a.add_argument("--start", help="Consume from earliest|latest message available in artefacts topic [latest]", type=str, default='latest')
+a.add_argument("--db", help="Mongo host/ip to save to [pi1]", type=str, default="pi1")
+a.add_argument("--port", help="TCP port to access mongo db [27017]", type=int, default=27017)
+a.add_argument("--dbname", help="Name on mongo DB to access [au_js]", type=str, default="au_js")
+args = a.parse_args()
+
+group = args.group
+start = 'latest'
+if len(group) < 1:
+    group = None
+    start = 'earliest'
+consumer = KafkaConsumer(args.topic, group_id=group, auto_offset_reset=start, consumer_timeout_ms=10000, 
+                         bootstrap_servers=args.bootstrap, value_deserializer=lambda m: json.loads(m.decode('utf-8')))
+mongo = pymongo.MongoClient(args.db, args.port)
+db = mongo[args.dbname]
 
 cnt = 0
 for message in consumer:
