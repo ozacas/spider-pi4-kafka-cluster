bdc66f7a61b5b42fb488dd69a45a7a939b45aa8e
Andrew Cassin <acassin999@gmail.com>
2020-03-22 11:12:43 +1100

    Initial checkin of control checking code

--- src/utils/features.py
+++ src/utils/features.py
@@ -1,8 +1,29 @@
 import subprocess
 import json
 import os
+import math 
+import hashlib
 from dataclasses import asdict
 from tempfile import NamedTemporaryFile
+
+# comes from MongoDB (db.statements_by_count_keys collection) but for performance is listed here
+normalised_ast_features_list =  [ "ArrayLiteral", "Assignment", "AstRoot", "Block", "BreakStatement", "CatchClause", "ConditionalExpression",
+        "ContinueStatement", "DoLoop", "ElementGet", "EmptyExpression", "EmptyStatement", "ExpressionStatement", "ForInLoop", "ForLoop",
+        "FunctionCall", "FunctionNode", "IfStatement", "InfixExpression", "KeywordLiteral", "Label", "LabeledStatement", "Name",
+        "NewExpression", "NumberLiteral", "ObjectLiteral", "ObjectProperty", "ParenthesizedExpression", "PropertyGet",
+        "RegExpLiteral", "ReturnStatement", "Scope", "StringLiteral", "SwitchCase", "SwitchStatement", "ThrowStatement",
+        "TryStatement", "UnaryExpression", "VariableDeclaration", "VariableInitializer", "WhileLoop", "WithStatement", "XmlLiteral", "XmlString" ]
+
+
+def safe_for_mongo(function_vector):
+   # returns a dict which has all keys made safe for insertion into mongoDB
+   # since $ for fields is not permissable in mongo, we replace with F$ 
+   d = {}
+   for k, v in function_vector.items():
+       if k.startswith('$'):
+           k = "F"+k
+       d[k] = v
+   return d
 
 def analyse_script(js, jsr, producer=None, java='/usr/bin/java', feature_extractor="/home/acas/src/pi-cluster-ansible-cfg-mgmt/src/extract-features.jar"):
    # save code to a file
@@ -21,14 +42,7 @@ def analyse_script(js, jsr, producer=None, java='/usr/bin/java', feature_extract
        ret = json.loads(process.stdout)
        ret.update(d)       # will contain url key
        ret.pop('id', None) # remove duplicate url entry silently
-       # since $ for fields is not permissable in mongo, we replace with F$ 
-       function_vector = ret.pop('calls_by_count') 
-       d = {}
-       for k, v in function_vector.items():
-            if k.startswith('$'):
-                k = "F"+k
-            d[k] = v
-       ret['calls_by_count'] = d
+       ret['calls_by_count'] = safe_for_mongo(ret.pop('calls_by_count'))
    elif producer:
        producer.send("feature-extraction-failures", d)
 
@@ -37,3 +51,44 @@ def analyse_script(js, jsr, producer=None, java='/usr/bin/java', feature_extract
 
    return ret
 
+def normalise_vector(ast_features):
+   # ensure return vector is standardised in name order with all values filled in
+   global normalised_ast_features_list
+   ret = []
+   for f in normalised_ast_features_list:
+       ret.append(ast_features.get(f, 0))  # vector must always have the same length with the keys in a consistent order for comparison
+
+   return ret
+
+def find_hash_match(db, input_features, control_url):
+   if db:
+       rec = db.javascript_controls.find_one({ 'origin': control_url })
+       if rec is None:
+           print("Could not find control {}".format(control_url))
+           return False
+       expected_sha256 = rec.get('sha256', None)    
+       actual_sha256 = input_features.get('sha256')
+       # lookup db if sha256 is not in kafka message? nah, not for now... TODO FIXME
+       return expected_sha256 != actual_sha256
+   return False
+ 
+def find_best_control(input_features, controls, ignore_i18n=True, max_distance=100.0, db=None): 
+   best_control = None
+   best_distance = float('Inf')
+   hash_match = False
+   input_vector = normalise_vector(input_features['statements_by_count'])
+   #print(input_vector)
+   for control in controls:
+       if ignore_i18n and '/i18n/' in control['origin']:
+           continue
+
+       control_vector = normalise_vector(control['statements_by_count'])
+       
+       dist = math.dist(input_vector, control_vector)
+       if dist < best_distance and dist <= max_distance:
+            best_control = control['origin']
+            best_distance = dist
+            if dist < 0.01:     # try for a hash match against a control?
+                hash_match = find_hash_match(db, input_features, best_control)
+
+   return (best_control, best_distance, hash_match)
---
+++ src/vet_js_against_controls.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+import pymongo
+import json
+import argparse
+from kafka import KafkaConsumer, KafkaProducer
+from utils.features import find_best_control
+
+a = argparse.ArgumentParser(description="Read analysis results feature vector topic and closely related control vector (if any)")
+a.add_argument("--bootstrap", help="Kafka bootstrap servers", type=str, default="kafka1")
+a.add_argument("--n", help="Read no more than N records from kafka [Inf]", type=int, default=float('Inf'))
+a.add_argument("--topic", help="Read analysis results from specified topic [analysis-results]", type=str, default="analysis-results") # NB: can only be this topic
+a.add_argument("--group", help="Use specified kafka consumer group to remember where we left off [vet-features]", type=str, default='vet-features')
+a.add_argument("--v", help="Debug verbosely", action="store_true")
+a.add_argument("--start", help="Consume from earliest|latest message available in artefacts topic [latest]", type=str, default='latest')
+a.add_argument("--db", help="Mongo host/ip to save to [pi1]", type=str, default="pi1")
+a.add_argument("--port", help="TCP port to access mongo db [27017]", type=int, default=27017)
+a.add_argument("--dbname", help="Name on mongo DB to access [au_js]", type=str, default="au_js")
+a.add_argument("--to", help="Save results to named topic [vet-results]", type=str, default="javascript-artefact-control-results")
+args = a.parse_args()
+
+group = args.group
+start = 'latest'
+if len(group) < 1:
+    group = None
+    start = 'earliest'
+consumer = KafkaConsumer(args.topic, group_id=group, auto_offset_reset=start, consumer_timeout_ms=10000,
+                         bootstrap_servers=args.bootstrap, value_deserializer=lambda m: json.loads(m.decode('utf-8')))
+producer = KafkaProducer(value_serializer=lambda m: json.dumps(m).encode('utf-8'), bootstrap_servers=args.bootstrap)
+mongo = pymongo.MongoClient(args.db, args.port)
+db = mongo[args.dbname]
+
+# 0. read controls once only
+controls = list(db.javascript_controls.find())
+
+n = 0
+for message in consumer:
+    best_control, best_dist, sha256_matched = find_best_control(message.value, controls, db=db)
+    origin_url = message.value.get('url', message.value.get('id'))
+    vetted = { "control_url": best_control, "control_dist": best_dist, "origin": origin_url, "sha256_matched": sha256_matched }
+    if args.v:
+        print(vetted)
+    n += 1
+    producer.send(args.to, { 'best_control': best_control, 'best_distance': best_dist, 'artefact': message.value })
+    db.vet_against_control.find_one_and_update({ 'origin': origin_url }, { "$set": vetted })
+
+    if n >= args.n:
+       if args.v:
+           print("Processed {} records.".format(n))
+       break
+consumer.close()
