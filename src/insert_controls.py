5dad8c3e11cf17ec9817ea65fbc1c6d7a83aa411
Andrew Cassin <acassin999@gmail.com>
2020-03-25 16:55:26 +1100

    Add MongoDB RBAC support (readWrite access required)

--- src/insert_controls.py
+++ src/insert_controls.py
@@ -6,7 +6,7 @@ import hashlib
 import requests
 from datetime import datetime
 from utils.features import analyse_script
-from utils.models import JavascriptArtefact
+from utils.models import JavascriptArtefact, Password
 from utils.cdnjs import CDNJS
 from tempfile import NamedTemporaryFile
 
@@ -15,16 +15,18 @@ a.add_argument("--v", help="Debug verbosely", action="store_true")
 a.add_argument("--db", help="Mongo host/ip to save to [pi1]", type=str, default="pi1")
 a.add_argument("--port", help="TCP port to access mongo db [27017]", type=int, default=27017)
 a.add_argument("--dbname", help="Name on mongo DB to access [au_js]", type=str, default="au_js")
+a.add_argument("--user", help="User to authenticate to MongoDB RBAC (readWrite access required)", type=str, required=True)
+a.add_argument("--password", help="Password for user (prompted if not supplied)", type=Password, default=Password.DEFAULT)
 a.add_argument("--family", help="Name of JS family eg. jquery", type=str, required=True)
 a.add_argument("--release", help="Name of release eg. 1.10.3a", type=str, default=None)
 a.add_argument("--variant", help="Only save artefacts which match variant designation eg. minimised [None]", type=str, default=None)
 a.add_argument("--java", help="Path to JVM executable [/usr/bin/java]", type=str, default="/usr/bin/java")
 a.add_argument("--extractor", help="Path to feature extractor JAR", type=str, default="/home/acas/src/pi-cluster-ansible-cfg-mgmt/src/extract-features.jar")
 a.add_argument("--list", help="List available assets, but do not save to DB", action="store_true")
-a.add_argument("--i18n", help="Save internationalised versions [False]", action="store_true", default=False)
+a.add_argument("--i18n", help="Save internationalised versions of JS [False]", action="store_true", default=False)
 args = a.parse_args()
 
-mongo = pymongo.MongoClient(args.db, args.port)
+mongo = pymongo.MongoClient(args.db, args.port, username=args.user, password=str(args.password))
 db = mongo[args.dbname]
 
 def save_control(url, family, version, variant):
