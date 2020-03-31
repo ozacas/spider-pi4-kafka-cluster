92274183825c2fe001439e6895436d3171988ec7
Andrew Cassin <acassin999@gmail.com>
2020-03-31 13:05:37 +1100

    setup consumer group correctly

--- src/etl_control_results.py
+++ src/etl_control_results.py
@@ -11,6 +11,8 @@ from urllib.parse import urlparse
 
 a = argparse.ArgumentParser(description="Peform ETL on JS control hits and dump non-normalised data into MongoDB ready for querying")
 a.add_argument("--topic", help="Kafka topic to get visited JS summary [javascript-artefact-control-results]", type=str, default='javascript-artefact-control-results')
+a.add_argument("--group", help="Use specified kafka consumer group to remember where we left off [etl-controls2mongo]", type=str, default='etl-controls2mongo')
+a.add_argument("--start", help="Consume from earliest|latest message available in control results topic [latest]", type=str, default='latest')
 a.add_argument("--bootstrap", help="Kafka bootstrap servers [kafka1]", type=str, default="kafka1")
 a.add_argument("--n", help="Read no more than N records from kafka [infinite]", type=int, default=1000000000)
 a.add_argument("--v", help="Debug verbosely", action="store_true")
@@ -20,13 +22,15 @@ a.add_argument("--dbname", help="Name on mongo DB to access [au_js]", type=str, 
 a.add_argument("--user", help="MongoDB RBAC username to use (readWrite access required)", type=str, required=True)
 a.add_argument("--password", help="MongoDB password for user (prompted if not supplied)", type=Password, default=Password.DEFAULT)
 a.add_argument("--threshold", help="Ignore control hits with ast_distance greater than this", type=float, default=50.0)
+a.add_argument("--v", help="Debug verbosely", action="store_true")
 args = a.parse_args()
 
-mongo = pymongo.MongoClient(args.db, args.port, username=args.user, password=str(args.password))
+mongo = pymongo.MongoClient(args.db, args.port, 
+                            username=args.user, password=str(args.password))
 db = mongo[args.dbname]
 
-start = 'earliest'
-consumer = KafkaConsumer(args.topic, bootstrap_servers=args.bootstrap, group_id=None, auto_offset_reset=start, consumer_timeout_ms=1000,
+consumer = KafkaConsumer(args.topic, bootstrap_servers=args.bootstrap, group_id=args.group, 
+                         auto_offset_reset=args.start, consumer_timeout_ms=1000,
                          value_deserializer=lambda m: json.loads(m.decode('utf-8')))
 
 def cleanup(*args):
@@ -38,15 +42,12 @@ def cleanup(*args):
     consumer.close()
     sys.exit(0)
 
-origins = { }
-controls= { }
-signal.signal(signal.SIGINT, cleanup)
-
 def get_function_call_vector(db, url):
     ret = db.count_by_function.find_one({ 'url': url })
     return ret
 
 def next_artefact(consumer, max, verbose):
+    global origins
     n = 0
     for message in consumer:
         rec = BestControl(**message.value)
@@ -57,29 +58,38 @@ def next_artefact(consumer, max, verbose):
         if n >= max:
             break
 
-for control in db.javascript_controls.find({}):
-    #print(control)
-    url = control.get('origin')
-    controls[url] = control
+def load_controls(db, verbose):
+    controls = {}
+    for control in db.javascript_controls.find({}):
+        #print(control)
+        url = control.get('origin')
+        controls[url] = control
+    if args.v:
+        print("Loaded {} controls.".format(len(controls)))
+    return controls
 
-if args.v:
-    print("Loaded {} controls.".format(len(controls)))
-
-
+signal.signal(signal.SIGINT, cleanup)
+origins = { }
+controls = load_controls(db, args.v)
 for best_control in filter(lambda c: c.ast_dist < args.threshold, next_artefact(consumer, args.n, args.v)):
     dist = best_control.ast_dist
 
     up = urlparse(best_control.origin_url)
     host = up.hostname
-    if not host:
+    if host is None:
        continue
     origins[host] = 1
     d = asdict(best_control)
     d.pop('diff_functions', None)
     fv_origin = get_function_call_vector(db, best_control.origin_url)
     if fv_origin is None:
-        print("WARNING: unable to find function call vector for {}".format(best_control.origin_url))
+        if args.v:
+            print("WARNING: unable to find function call vector for {}".format(best_control.origin_url))
         continue
+    else:
+        if args.v:
+            print("Got function call vector for {}".format(best_control.origin_url))
+        # FALLTHRU
 
     u = best_control.control_url
     fv_control = controls[u].get('calls_by_count')
