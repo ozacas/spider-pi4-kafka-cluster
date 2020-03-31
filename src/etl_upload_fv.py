56655b4bcaf3b44902c9b083987b89efbfa1b40c
Andrew Cassin <acassin999@gmail.com>
2020-03-31 22:07:00 +1100

    Add customisable wait for new feature vectors before exiting. Default 10s

--- src/etl_upload_fv.py
+++ src/etl_upload_fv.py
@@ -14,19 +14,20 @@ a.add_argument("--bootstrap", help="Kafka bootstrap servers", type=str, default=
 a.add_argument("--topic", help="Read analysis results from specified topic [analysis-results]", type=str, default="analysis-results") # NB: can only be this topic
 a.add_argument("--group", help="Use specified kafka consumer group to remember where we left off [features2mongo]", type=str, default='features2mongo')
 a.add_argument("--start", help="Consume from earliest|latest message available in artefacts topic [latest]", type=str, default='latest')
-a.add_argument("--n", help="Read no more than N records from kafka [Inf]", type=int, default=1000000000)
+a.add_argument("--n", help="Read no more than N records from kafka [Inf]", type=float, default=float('Inf'))
 a.add_argument("--db", help="Mongo host/ip to save to [pi1]", type=str, default="pi1")
 a.add_argument("--port", help="TCP port to access mongo db [27017]", type=int, default=27017)
 a.add_argument("--dbname", help="Name on mongo DB to access [au_js]", type=str, default="au_js")
 a.add_argument("--user", help="MongoDB RBAC username to use (readWrite access required)", type=str, required=True)
 a.add_argument("--password", help="MongoDB password for user", type=Password, default=Password.DEFAULT)
+a.add_argument("--wait-for", help="Wait for at most X seconds for new feature vectors before exiting [10]", type=int, default=10)
 a.add_argument("--v", help="Debug verbosely", action="store_true")
 args = a.parse_args()
 
 group = args.group
 if len(group) < 1:
     group = None
-consumer = KafkaConsumer(args.topic, group_id=group, auto_offset_reset=start, consumer_timeout_ms=10000, 
+consumer = KafkaConsumer(args.topic, group_id=group, auto_offset_reset=args.start, consumer_timeout_ms= args.wait_for * 1000, 
                          bootstrap_servers=args.bootstrap, value_deserializer=lambda m: json.loads(m.decode('utf-8')))
 mongo = pymongo.MongoClient(args.db, args.port, username=args.user, password=str(args.password))
 db = mongo[args.dbname]
@@ -43,17 +44,20 @@ def cleanup(*args):
  
 signal.signal(signal.SIGINT, cleanup)
 for message in consumer:
-    u = message.value.get('id')
-    d = { 'url': u, 'origin': message.value.get('origin', None) }
-    d.update(**message.value.get('statements_by_count')) 
+    m = message.value
+    u = m.get('id')
+    d = { 'url': u, 'origin': m.get('origin', None) }
+    d.update(**m.get('statements_by_count')) 
     db.statements_by_count.insert_one(d)
-    d = { 'url': u, 'origin': message.value.get('origin', None) }
-    calls = safe_for_mongo(message.value.get('calls_by_count'))
+    d = { 'url': u, 'origin': m.get('origin', None) }
+    calls = safe_for_mongo(m.get('calls_by_count'))
     calls.pop('_id', None)           # not wanted since already in d
     d.update(calls)
     db.count_by_function.insert_one(d)
     cnt += 1
     if cnt % 1000 == 0:
         print("Processed {} records.".format(cnt))
+    if cnt >= args.n:
+        break
 cleanup()
 exit(0)
