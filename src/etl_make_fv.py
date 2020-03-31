e6498956ce3030ccf14e0c7bceb8fc6508fb4718
Andrew Cassin <acassin999@gmail.com>
2020-03-31 22:35:11 +1100

    Cleanup filter() usage. Fix args.start to be used correctly by consumer

--- src/etl_make_fv.py
+++ src/etl_make_fv.py
@@ -24,15 +24,13 @@ a.add_argument("--to", help="Send output to specified topic [analysis-results]",
 a.add_argument("--bootstrap", help="Kafka bootstrap servers [kafka1]", type=str, default="kafka1")
 a.add_argument("--n", help="Read no more than N records from kafka [infinite]", type=int, default=1000000000)
 a.add_argument("--group", help="Use specified kafka consumer group to find correct topic position [javascript-analysis]", type=str, default='javascript-analysis')
+a.add_argument("--start", help="Start at earliest|latest available message [earliest]", type=str, default='earliest')
 a.add_argument("--v", help="Debug verbosely", action="store_true")
 a.add_argument("--java", help="Java client used to run the program", type=str, default="/usr/bin/java")
 a.add_argument("--extractor", help="JAR file to perform the feature calculation for each JS artefact", type=str, default="/home/acas/src/extract-features.jar")
 args = a.parse_args()
 
-start = 'latest'
-if len(args.group) < 1:
-    start = 'earliest'
-consumer = KafkaConsumer(args.topic, bootstrap_servers=args.bootstrap, group_id=args.group, auto_offset_reset=start,
+consumer = KafkaConsumer(args.topic, bootstrap_servers=args.bootstrap, group_id=args.group, auto_offset_reset=args.start,
                          value_deserializer=lambda m: json.loads(m.decode('utf-8')), max_poll_interval_ms=30000000) # crank max poll to ensure no kafkapython timeout
 producer = KafkaProducer(value_serializer=lambda m: json.dumps(m).encode('utf-8'), bootstrap_servers=args.bootstrap)
 mongo = pymongo.MongoClient(args.mongo_host, args.mongo_port, username=args.user, password=str(args.password))
@@ -62,6 +60,9 @@ def next_artefact(consumer, max):
     for msg in consumer:
         d = msg.value
         d['content_type'] = d['content-type']
+        if 'javascript' not in d['content_type']:
+            continue
+
         del d['content-type']
         yield JavascriptArtefact(**d)
         # done enough per user request?
@@ -70,8 +71,8 @@ def next_artefact(consumer, max):
             break
 
 # we want only artefacts which are not cached and are JS (subject to maximum record limits)
-uncached_artefacts = filter(lambda a: a.url not in cache, next_artefact(consumer, args.n))
-for jsr in filter(lambda a: 'javascript' in a.content_type, uncached_artefacts): 
+uncached_js_artefacts = filter(lambda a: not a.url in cache, next_artefact(consumer, args.n))
+for jsr in uncached_js_artefacts:
     # eg.  {'url': 'https://XXXX.asn.au/', 'size_bytes': 294, 'inline': True, 'content-type': 'text/html; charset=UTF-8', 
     #       'when': '2020-02-06 02:51:46.016314', 'sha256': 'c38bd5db9472fa920517c48dc9ca7c556204af4dee76951c79fec645f5a9283a', 
     #        'md5': '4714b9a46307758a7272ecc666bc88a7', 'origin': 'XXXX' }  NB: origin may be none for old records (sadly)
