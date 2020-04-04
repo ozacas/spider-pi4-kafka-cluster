79a7a33f80ad236a5ccba6ce7233d48f0106d011
Andrew Cassin <acassin999@gmail.com>
2020-04-05 09:56:31 +1000

    Use consistent naming scheme for JS controls related ETL scripts. First cut at support for JSDelivr CDN support. Uploading controls doesnt do dupe hashes anymore, to minimise number of controls

--- src/etl_control_upload.py
+++ src/etl_control_upload.py
@@ -29,7 +29,7 @@ args = a.parse_args()
 mongo = pymongo.MongoClient(args.db, args.port, username=args.user, password=str(args.password))
 db = mongo[args.dbname]
 
-def save_control(url, family, version, variant, force=False, refuse_hashes=set()):
+def save_control(url, family, version, variant, force=False, refuse_hashes=set(), provider=''):
    resp = requests.get(url)
    content = resp.content
 
@@ -38,17 +38,18 @@ def save_control(url, family, version, variant, force=False, refuse_hashes=set()
                              inline=False, content_type='text/javascript', size_bytes=len(content))
    if jsr.sha256 in refuse_hashes and not force:
        print("Refusing to update existing control as dupe: {}".format(jsr))
-       return
+       return jsr
 
    ret = analyse_script(content, jsr, producer=None, java=args.java, feature_extractor=args.extractor)
    if ret is None:
        raise ValueError('Could not analyse script {}'.format(jsr.url))
-   ret.update({ 'family': family, 'release': version, 'variant': variant, 'origin': url })
+   ret.update({ 'family': family, 'release': version, 'variant': variant, 'origin': url, 'provider': provider })
    #print(ret)
    # NB: only one control per url/family pair (although in theory each CDN url is enough on its own)
    resp = db.javascript_controls.find_one_and_update({ 'origin': url, 'family': family }, { "$set": ret }, upsert=True)
    if args.v:
        print(resp) 
+   return jsr
 
 provider = CDNJS() if args.provider == "cdnjs" else JSDelivr()
 existing_control_hashes = set(db.javascript_controls.distinct('sha256'))
@@ -57,7 +58,8 @@ for url, family, variant, version in provider.fetch(args.family, args.variant, a
        print("Found artefact: {}".format(url))
     if not args.list:
        try: 
-           save_control(url, family, variant, version, refuse_hashes=existing_control_hashes)
+           artefact = save_control(url, family, variant, version, refuse_hashes=existing_control_hashes, provider=args.provider)
+           existing_control_hashes.add(artefact.sha256)
        except Exception as e:
            print(str(e))
     
