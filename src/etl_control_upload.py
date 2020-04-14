7435c15db89f1f43f8ee5a90ea1481d9584d3180
Andrew Cassin <acassin999@gmail.com>
2020-04-14 13:38:55 +1000

    Add support for re-calculating all existing controls

--- src/etl_control_upload.py
+++ src/etl_control_upload.py
@@ -23,16 +23,22 @@ a.add_argument("--release", help="Name of release eg. 1.10.3a", type=str, defaul
 a.add_argument("--variant", help="Only save artefacts which match variant designation eg. minimised [None]", type=str, default=None)
 a.add_argument("--java", help="Path to JVM executable [/usr/bin/java]", type=str, default="/usr/bin/java")
 a.add_argument("--extractor", help="Path to feature extractor JAR", type=str, default="/home/acas/src/pi-cluster-ansible-cfg-mgmt/src/extract-features.jar")
-a.add_argument("--list", help="List available assets, but do not save to DB", action="store_true")
 a.add_argument("--i18n", help="Save internationalised versions of JS [False]", action="store_true", default=False)
 a.add_argument("--provider", help="Specify CDN provider [cdnjs]", type=str, choices=['cdnjs', 'jsdelivr'])
-a.add_argument("--update", help="Only update existing controls by re-fetching from CDN provider", action="store_true")
+group = a.add_mutually_exclusive_group()
+group.add_argument("--list", help="List available assets, but do not save to DB", action="store_true")
+group.add_argument("--update-all", help="Only update existing controls by re-fetching from CDN provider", action="store_true")
 args = a.parse_args()
 
 mongo = pymongo.MongoClient(args.db, args.port, username=args.user, password=str(args.password))
 db = mongo[args.dbname]
 
-def save_control(url, family, version, variant, force=False, refuse_hashes=set(), provider=''):
+def save_control(url, family, version, variant, force=False, refuse_hashes=None, provider=''):
+   """
+   Update all control related data. Note callers must supply refuse_hashes (empty set) or an error will result
+
+   Returns JavascriptArtefact representing control which has had its state updated into MongoDB
+   """
    resp = requests.get(url)
    content = resp.content
 
@@ -51,7 +57,7 @@ def save_control(url, family, version, variant, force=False, refuse_hashes=set()
    # NB: only one control per url/family pair (although in theory each CDN url is enough on its own)
    resp = db.javascript_controls.find_one_and_update({ 'origin': url, 'family': family }, 
                                                      { "$set": ret }, upsert=True)
-   resp = db.javascript_control_code.find_one_and_update({ 'origin': url }, 
+   db.javascript_control_code.find_one_and_update({ 'origin': url }, 
                                                      { "$set": { 'origin': url, 'code': Binary(content), 
                                                        "last_updated": jsr.when } }, upsert=True)
    # this code comes from etl_control_fix_magnitude, which means we dont need to run it separately
@@ -59,23 +65,27 @@ def save_control(url, family, version, variant, force=False, refuse_hashes=set()
    sum_of_function_calls = sum(ret['calls_by_count'].values())
    vs = JavascriptVectorSummary(origin=url, sum_of_ast_features=total_sum,
                                  sum_of_functions=sum_of_function_calls, last_updated=jsr.when)
-   db.javascript_controls_summary.find_one_and_update({ 'origin': url }, { "$set": asdict(vs) }, upsert = True)
+   db.javascript_controls_summary.find_one_and_update({ 'origin': url }, { "$set": asdict(vs) }, upsert=True)
    return jsr
 
-existing_control_hashes = set(db.javascript_controls.distinct('sha256'))
 
-if args.update:
-    controls_to_save = [(ret['origin'], ret['family'], ret['variant'], ret['version'], ret['provider']) for ret in db.javascript_controls.find()]
+if args.update_all:
+    controls_to_save = [(ret['origin'], ret['family'], ret['variant'], ret['variant'], ret.get('provider', '')) for ret in db.javascript_controls.find()]
+    existing_control_hashes = set()
+    print("Updating all MongoDB data for {} JS controls.".format(len(controls_to_save)))
 else:
     fetcher = CDNJS() if args.provider == "cdnjs" else JSDelivr()
+    existing_control_hashes = set(db.javascript_controls.distinct('sha256'))
     controls_to_save = fetcher.fetch(args.family, args.variant, args.release, ignore_i18n=not args.i18n, provider=args.provider)
+
 for url, family, variant, version, provider in controls_to_save:
     if args.v or args.list:
        print("Found artefact: {}".format(url))
     if not args.list:
        try: 
            artefact = save_control(url, family, variant, version, refuse_hashes=existing_control_hashes, provider=provider)
-           existing_control_hashes.add(artefact.sha256)
+           if not args.update_all:
+               existing_control_hashes.add(artefact.sha256)
            if args.v:
                print(artefact)
        except Exception as e:
