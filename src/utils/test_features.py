f0a0ab2cf0dd7a3d2d7d9bc345e5aabfdf2d8bfa
Andrew Cassin <acassin999@gmail.com>
2020-04-20 14:21:44 +1000

    add unit tests for calc_function_dist()

--- src/utils/test_features.py
+++ src/utils/test_features.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 import pytest
-from utils.features import safe_for_mongo, as_url_fields, compute_distance, calculate_ast_vector
+from utils.features import safe_for_mongo, as_url_fields, compute_distance, calculate_ast_vector, calc_function_dist
 
 def test_safe_for_mongo():
    assert safe_for_mongo({ 'a': 0, '$': 1 }) == { 'a': 0, 'F$': 1 }
@@ -15,6 +15,19 @@ def test_compute_distance():
    assert compute_distance([112, 33], [99, 12]) >= 246.98
    dist = compute_distance([112, 33], [99, 12], short_vector_penalty=False)
    assert dist >= 24.6981 and dist <= 24.6982
+
+def test_compute_function_dist():
+   a = { "foo": 3, "bar": 7, "baz": 1 }
+   b = { "crud": 20, "foo": 3, "bar": 7 }
+   dist, diff_functions = calc_function_dist(a, b)
+   assert dist >= 10.01 and dist <= 10.02 
+   sorted(diff_functions)
+   assert diff_functions == ['baz', 'crud']
+   a = { "tmp": 1 }
+   b = { "tmp": 2 }
+   dist, diff_functions = calc_function_dist(a, b)
+   assert dist > 0.0
+   assert diff_functions == ['tmp']
 
 def test_compute_ast_vector():
    d = { "ArrayLiteral": 10, "Assignment": 7, "AstRoot": 1 }
