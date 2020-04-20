bb8c1b75a5e217888ff0411fa689587418b0d638
Andrew Cassin <acassin999@gmail.com>
2020-04-20 17:44:53 +1000

    Use central code for setting up CLI arguments to save space and make it easier to maintain

--- src/utils/misc.py
+++ src/utils/misc.py
@@ -1,5 +1,6 @@
 import signal
 import os
+from utils.models import Password
 from urllib.parse import urlparse
 
 def setup_signals(callback):
@@ -40,3 +41,46 @@ def as_priority(url, up):
       penalty = penalty + 1
    return priority + penalty
 
+def add_extractor_arguments(args, default_extractor="/home/acas/src/extract-features.jar"):
+   args.add_argument("--java", 
+                     help="Path to JVM executable [/usr/bin/java]", 
+                     type=str, default="/usr/bin/java")
+   args.add_argument("--extractor", 
+                      help="Path to feature extractor JAR [{}]".format(default_extractor), 
+                      type=str, 
+                      default=default_extractor)
+
+def add_mongo_arguments(args, default_host='pi1', default_port=27017, default_db='au_js', default_access='read-only'):
+   args.add_argument("--db", 
+                     help="Mongo host/ip to save to [{}]".format(default_host), 
+                     type=str, default=default_host)
+   args.add_argument("--port", 
+                     help="TCP port to access mongo db [{}]".format(str(default_port)), 
+                     type=int, default=default_port)
+   args.add_argument("--dbname", 
+                     help="Name on mongo DB to access [{}]".format(default_db), 
+                     type=str, default=default_db)
+   args.add_argument("--dbuser", 
+                     help="MongoDB RBAC username to use ({} access required)".format(default_access), type=str, required=True)
+   args.add_argument("--dbpassword", 
+                     help="MongoDB password for user (prompted if not supplied)", type=Password, default=Password.DEFAULT)
+
+def add_kafka_arguments(args, producer=False, consumer=True, default_from='', default_group='', default_to=''):
+   args.add_argument("--bootstrap", 
+                  help="Kafka bootstrap servers [kafka1]", 
+                  type=str, default="kafka1")
+   if consumer:
+       args.add_argument("--consume-from", 
+                         help="Kafka topic to read input data from [{}]".format(default_from), 
+                         type=str, default=default_from)
+       args.add_argument("--group", 
+                      help="Use specified kafka consumer group to remember where we left off [{}]".format(default_group), 
+                      type=str, default=default_group)
+       args.add_argument("--start", 
+                      help="Consume from earliest|latest message available in from topic [latest]", 
+                      type=str, default='latest', choices=('earliest', 'latest'))
+   if producer:
+       args.add_argument("--to", help="Output records to specified topic [{}]".format(default_to), type=str, default=default_to)
+
+def add_debug_arguments(args):
+   args.add_argument("--v", help="Debug verbosely", action="store_true")
