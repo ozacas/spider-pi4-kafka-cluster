c8ecc8128eca1f863bfdb859ab418e159f932ba7
Andrew Cassin <acassin999@gmail.com>
2020-04-21 13:22:32 +1000

    Add unit test for analyse_script() for JS that works with rhino

--- src/utils/test_features.py
+++ src/utils/test_features.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import pytest
-from utils.features import safe_for_mongo, as_url_fields, compute_distance, calculate_ast_vector, calc_function_dist
+from utils.features import safe_for_mongo, as_url_fields, compute_distance, calculate_ast_vector, calc_function_dist, analyse_script
+from utils.models import JavascriptArtefact
 
 def test_safe_for_mongo():
    assert safe_for_mongo({ 'a': 0, '$': 1 }) == { 'a': 0, 'F$': 1 }
@@ -34,3 +35,22 @@ def test_compute_ast_vector():
    tuple = calculate_ast_vector(d) 
    assert tuple == ([10, 7, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 18) 
    assert compute_distance(tuple[0], tuple[0]) <= 0.001
+
+def test_analyse_script(pytestconfig):
+   testjs = "{}/src/test-javascript/banners.js".format(pytestconfig.rootdir)
+   with open(testjs, "rb") as fp:
+      jsr = JavascriptArtefact(url="file:{}".format(testjs), origin=None, sha256='XXX', md5='XXX')
+      json, failed, stderr = analyse_script(fp.read(), jsr, feature_extractor="{}/src/extract-features.jar".format(pytestconfig.rootdir))
+      assert not failed
+      assert 'when' in json and isinstance(json['when'], str) and len(json['when']) > 0
+      json.pop('when', None)
+      assert json == {'statements_by_count': {'StringLiteral': 32, 'VariableInitializer': 1, 'KeywordLiteral': 2, 'AstRoot': 1, 'ObjectLiteral': 2, 'ObjectProperty': 12, 'Name': 14, 'NumberLiteral': 5, 'ArrayLiteral': 9, 'VariableDeclaration': 1, 'NewExpression': 1}, 
+'url': 'file:/home/acas/src/pi-cluster-ansible-cfg-mgmt/src/test-javascript/banners.js', 
+'sha256': 'XXX', 
+'md5': 'XXX', 
+'inline': False, 
+'content_type': 'text/javascript', 
+'size_bytes': 0, 
+'origin': None, 
+'calls_by_count': {}, 
+'literals_by_count': {'0': 1, '220': 1, '//www.spec-net.com.au/banners/200129_Rollashield.jpg': 1, 'https://www.rollashieldshutters.com.au/bushfire-shutters/': 1, '470': 1, '500': 1, '//www.spec-net.com.au/banners/200129_GCP-Applied-Technologies.jpg': 1, '5000': 1, 'fadeshow1': 1, 'https://gcpat.com.au/en-gb/news/blog': 1, '//www.spec-net.com.au/banners/200129_RMS.jpg': 1, '//www.spec-net.com.au/banners/200129_Tornex-Door-Systems.jpg': 1, 'auto': 1, 'https://www.spec-net.com.au/press/0120/dpp_290120/Elegant-Bathroom-Radiators-AGAVE-by-dPP-Hydronic-Heating': 1, 'https://www.rmsmarble.com/vetrazzo-slabs/': 1, '//www.spec-net.com.au/banners/200129_dPP-Hydronic-Heating.jpg': 1, 'Elegant Bathroom Radiators - AGAVE by dPP Hydronic Heating': 1, 'GCP Preprufe Plus Seamless Protection of Underground Structures': 1, 'A Gorgeous Staircase Featuring AWIS Wrought Iron Components': 1, 'Electrical Operated, Triple Glazed Roof Hatches by Gorter Hatches': 1, 'https://www.gortergroup.com/au/products/roof-hatches/rhtg-glazed.html': 1, '//www.spec-net.com.au/banners/200129_AWIS.jpg': 1, 'Handcrafted Recycled Glass Benchtops - Vetrazzo by RMS': 1, 'Single & Double Sliding Track Doors from Tornex Door Systems': 1, '//www.spec-net.com.au/banners/200205_Gorter-Hatches.jpg': 1, 'always': 1, 'https://www.tornex.com.au/': 1, 'AS3959-2009 Compliant BAL FZ Bushfire Shutters from Rollashield': 1, 'http://artisticwroughtiron.com.au/': 1}} 
