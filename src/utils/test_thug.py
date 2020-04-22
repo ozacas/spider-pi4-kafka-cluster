795201e14060dcd13486c41581359cdcec29dda2
Andrew Cassin <acassin999@gmail.com>
2020-04-22 21:05:37 +1000

    Basic thug log integration test

--- src/utils/test_thug.py
+++ src/utils/test_thug.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
 import pymongo
-import mock
+from unittest.mock import Mock
 from utils.geo import AustraliaGeoLocator
 from utils.thug_parse import ThugLogParser
 
 def test_thug_parser(pytestconfig):
     mongo = { }
-    mongo['thug'] = mock.Mock()
+    mongo['thug'] = Mock()
     ThugLogParser(context={ 'url': 'foo' }, 
               au_locator=AustraliaGeoLocator(),
               mongo=mongo).parse("{}/data/example10.log".format(pytestconfig.rootdir))
-    print(mongo['thug'].called)
-    print(mongo['thug'].method_list)
+    m = mongo['thug'] 
+    assert len(m.method_calls) == 1
+    name, args, kwargs = m.method_calls[0] 
+    assert name == "thug_log.insert_one" 
+    assert len(args) == 1
+    assert len(kwargs) == 0
