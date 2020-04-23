76e0c797da7c7a1f7eb4cc5f38bf1d24f52cb6a0
Andrew Cassin <acassin999@gmail.com>
2020-04-23 19:41:35 +1000

    Refactor save_* from etl_upload_artefacts into io with test cases

--- src/utils/io.py
+++ src/utils/io.py
@@ -1,5 +1,8 @@
 from utils.features import safe_for_mongo
 from utils.models import JavascriptArtefact
+from collections import namedtuple
+import pymongo
+import hashlib
 
 def save_ast_vector(db, jsr: JavascriptArtefact, ast_vector, js_id: str=None):
    assert ast_vector is not None
@@ -27,3 +30,37 @@ def next_artefact(iterable, max: float=float('Inf'), verbose: bool=False, filter
             print("Processed {} records.".format(n))
         if n >= max:
             break
+
+def artefact_tuple():
+   ret = namedtuple('Artefact', 'url origin host checksum path when')
+   ret.__lt__ = lambda self, other: self.checksum < other.checksum
+   return ret
+
+def save_url(db, artefact):
+   result = db.urls.insert_one({ 'url': artefact.url, 'last_visited': artefact.when, 'origin': artefact.origin })
+   return result.inserted_id
+
+def save_script(db, artefact, script: bytes):
+   # NB: we work hard here to avoid mongo calls which will cause performance problems (hashing too)
+
+   # compute hashes to search for
+   sha256 = hashlib.sha256(script).hexdigest()
+   md5 = hashlib.md5(script).hexdigest()
+   if md5 != artefact.checksum:
+       raise ValueError("Expected MD5 and MD5 hash do not match: {} {} != {}".format(artefact.url, md5, artefact.checksum))
+
+   # check to see if in mongo already
+   url_id = save_url(db, artefact)
+
+   script_len = len(script)
+   key = { 'sha256': sha256, 'md5': md5, 'size_bytes': script_len }  
+   value = key.copy() # NB: shallow copy is sufficient for this application
+   value.update({ 'code': script })
+
+   s = db.scripts.find_one_and_update(key, { '$set': value }, 
+                                      upsert=True, 
+                                      projection={ 'code': False, '_id': True },
+                                      return_document=pymongo.ReturnDocument.AFTER)
+   db.script_url.insert_one( { 'url_id': url_id, 'script': s.get(u'_id') })
+
+   return key
--- src/utils/test_io.py
+++ src/utils/test_io.py
@@ -45,3 +45,39 @@ def test_next_artefact():
    results = list(next_artefact(d, filter_cb=lambda v: v['ast_dist'] < 0.01 or v['function_dist'] < 0.01))
    assert len(results) == 1
    assert results[0] == j2.value
+
+def test_artefact_tuple():
+   type = artefact_tuple()
+   t = type(url='http://blahblah', path='foo/bar/baz.js', checksum='XXX', host='pi5', origin='http://some.page/here.html', when='2019-20...')
+   assert t.when is not None and isinstance(t.when, str)
+   assert t.url == 'http://blahblah'
+   assert t.path == 'foo/bar/baz.js'
+   assert t.checksum == 'XXX'
+   assert t.host == 'pi5'
+   assert t.origin == 'http://some.page/here.html'
+
+def test_save_url():
+   tuple = artefact_tuple()
+   t = tuple(url='http://www.blah.blah/foo.js', path='full/123yu4u43yu4y43.js',
+             checksum='1a...', host='pi2', origin='http://www.blah.blah/index.html', when='2019-10-10')
+   m = Mock() 
+   ret = save_url(m, t)
+   assert ret is not None # TODO FIXME: better test than this...
+   assert len(m.method_calls) == 1
+   name, args, kwargs = m.method_calls[0]
+   assert name == 'urls.insert_one'
+   assert len(args) == 1
+   assert args[0] == {'url': 'http://www.blah.blah/foo.js', 'last_visited': '2019-10-10', 'origin': 'http://www.blah.blah/index.html'}
+   assert len(kwargs) == 0
+
+def test_save_script():
+   m = Mock()
+   tuple = artefact_tuple()
+   # checksum corresponds to the rubbish code below...
+   jsr = tuple(url='X', path='full/foo.js', checksum='f8f4392b9ce13de380ecdbe256030807', host='pi1', origin='foo.html', when='2020-04-20')
+   ret = save_script(m, jsr, 'console.write.(blah blah)'.encode())
+   assert ret is not None
+   assert isinstance(ret, dict)
+   assert 'sha256' in ret
+   assert 'md5' in ret
+   assert 'size_bytes' in ret 
