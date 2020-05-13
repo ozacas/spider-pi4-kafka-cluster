0a40548ee11a11ca3e6d486fc7795a580183f15c
Andrew Cassin <acassin999@gmail.com>
2020-05-14 09:07:32 +1000

    Add control_url (even though it will often be blank) to give some indicator where the hit came from

--- src/etl_publish_poor_hits.py
+++ src/etl_publish_poor_hits.py
@@ -53,7 +53,9 @@ if __name__ == "__main__":
        # 2) they might suggest other ways which have to be handled by the system
        # so here we want to provide enough data (even for the bad hit) to enable build-data-matrix-from-etl.py to 
        # be able to efficiently query for clusters of bad hits
-       d = { 'ast_dist': bad_hit.ast_dist, 'function_dist': bad_hit.function_dist, 'origin_url': bad_hit.origin_url, 'cited_on': bad_hit.cited_on }
+       d = { 'control_url': bad_hit.control_url, 'ast_dist': bad_hit.ast_dist, 
+             'function_dist': bad_hit.function_dist, 'origin_url': bad_hit.origin_url, 
+             'cited_on': bad_hit.cited_on }
        d.update(as_url_fields(bad_hit.origin_url, prefix='origin'))
        d.update(as_url_fields(bad_hit.cited_on, prefix='cited_on'))
        # and finally compute hashes for the content for clustering 
