377f9372afb20a65e943ecadddf87f8485568ee1
Andrew Cassin <acassin999@gmail.com>
2020-05-21 08:22:20 +1000

    Update as_priority() unit test to new API

--- src/utils/test_misc.py
+++ src/utils/test_misc.py
@@ -5,11 +5,6 @@ from utils.misc import as_priority
 from urllib.parse import urlparse
 
 def test_as_priority():
-   pri = as_priority('https://www.google.com', None)
+   pri = as_priority(urlparse('https://www.google.com'))
    assert pri is not None and pri <= 2
-
-   # avoid re-parsing url's if we can
-   up = urlparse('https://www.google.com')
-   t2 = as_priority('https://www.google.com', up)
-   assert t2 is not None and t2 == pri
 
