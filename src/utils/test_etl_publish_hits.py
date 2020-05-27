7b68687896f5d8482599c6419f6aa4fd60b380b7
Andrew Cassin <acassin999@gmail.com>
2020-05-27 21:05:49 +1000

    Neater layout of process_hit() expected results

--- src/utils/test_etl_publish_hits.py
+++ src/utils/test_etl_publish_hits.py
@@ -22,8 +22,8 @@ def test_process_hit_success():
                     sha256_matched=True, diff_functions='', ast_dist=0.0, literal_dist=0.0, function_dist=0.0)
    all_controls = { control_url: { 'control_url': control_url, 'literals_by_count': { 'a': 1 } }}
    assert process_hit(db, all_controls, bc, producer) == True
-   assert db.method_calls == [mock.call.count_by_function.find_one({'url': 'https://some.web.site/somewhere.js'}),
-                              mock.call.etl_hits.insert_one({'control_url': 'https://cdn.com/path/to/artefact.js', 
+   expected_find_call = mock.call.count_by_function.find_one({'url': 'https://some.web.site/somewhere.js'})
+   expected_insert_call = mock.call.etl_hits.insert_one({'control_url': 'https://cdn.com/path/to/artefact.js', 
                                                         'origin_url': 'https://some.web.site/somewhere.js', 'sha256_matched': True, 
                                                         'ast_dist': 0.0, 'function_dist': 0.0, 'cited_on': 'https://some.web.site/somewhere.html', 
                                                         'origin_js_id': None, 'literal_dist': 0.0, 'xref': None, 'literals_not_in_control': -1, 
@@ -31,6 +31,16 @@ def test_process_hit_success():
                                                         'origin_host': 'some.web.site', 'origin_has_query': False, 'origin_port': 443, 
                                                         'origin_scheme': 'https', 'origin_path': '/somewhere.js', 'cited_on_host': 'some.web.site', 
                                                         'cited_on_has_query': False, 'cited_on_port': 443, 'cited_on_scheme': 'https', 
-                                                        'cited_on_path': '/somewhere.html', 'control_family': None, 'diff_functions': []})]
-   assert producer.method_calls == [mock.call.send('etl-good-hits', {'control_url': 'https://cdn.com/path/to/artefact.js', 'origin_url': 'https://some.web.site/somewhere.js', 'sha256_matched': True, 'ast_dist': 0.0, 'function_dist': 0.0, 'cited_on': 'https://some.web.site/somewhere.html', 'origin_js_id': None, 'literal_dist': 0.0, 'xref': None, 'literals_not_in_control': -1, 'literals_not_in_origin': -1, 'n_diff_literals': -1, 'diff_literals': '', 'origin_host': 'some.web.site', 'origin_has_query': False, 'origin_port': 443, 'origin_scheme': 'https', 'origin_path': '/somewhere.js', 'cited_on_host': 'some.web.site', 'cited_on_has_query': False, 'cited_on_port': 443, 'cited_on_scheme': 'https', 'cited_on_path': '/somewhere.html', 'control_family': None, 'diff_functions': []})]
+                                                        'cited_on_path': '/somewhere.html', 'control_family': None, 'diff_functions': []})
+   assert db.method_calls == [expected_find_call, expected_insert_call]
+   assert producer.method_calls == [mock.call.send('etl-good-hits', 
+                                       {'control_url': 'https://cdn.com/path/to/artefact.js', 
+                                        'origin_url': 'https://some.web.site/somewhere.js', 'sha256_matched': True, 
+                                        'ast_dist': 0.0, 'function_dist': 0.0, 'cited_on': 'https://some.web.site/somewhere.html', 
+                                        'origin_js_id': None, 'literal_dist': 0.0, 'xref': None, 'literals_not_in_control': -1, 
+                                        'literals_not_in_origin': -1, 'n_diff_literals': -1, 'diff_literals': '', 
+                                        'origin_host': 'some.web.site', 'origin_has_query': False, 'origin_port': 443, 
+                                        'origin_scheme': 'https', 'origin_path': '/somewhere.js', 'cited_on_host': 'some.web.site', 
+                                        'cited_on_has_query': False, 'cited_on_port': 443, 'cited_on_scheme': 'https', 
+                                        'cited_on_path': '/somewhere.html', 'control_family': None, 'diff_functions': []})]
 
