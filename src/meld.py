f89ec70eeb1464c7da24e8cf914652a944e4ebe7
Andrew Cassin <acassin999@gmail.com>
2020-05-29 12:55:39 +1000

    Update meld to support use of xref to improve functionality significantly

--- src/meld.py
+++ src/meld.py
@@ -12,16 +12,6 @@ from utils.misc import add_mongo_arguments, add_debug_arguments
 from utils.models import JavascriptArtefact
 from datetime import datetime
 
-a = argparse.ArgumentParser(description="Run meld on the chosen URL as its best control, after JS beatification (optional)")
-add_mongo_arguments(a, default_access='read-only', default_user='ro')
-add_debug_arguments(a)
-a.add_argument("--url", help="URL of Javascript to investigate (code fetched from DB, not internet)", type=str, required=True)
-a.add_argument("--diff", help="Diff program to run [/usr/bin/meld]", type=str, default="/usr/bin/meld")
-a.add_argument("--beautifier", help="JS Beautifier to run [/usr/local/bin/js-beautify]", type=str, default="/usr/local/bin/js-beautify")
-args = a.parse_args()
-
-mongo = pymongo.MongoClient(args.db, args.port, username=args.dbuser, password=str(args.dbpassword))
-db = mongo[args.dbname]
 
 def save_control(db, filename, control_url):
    script = db.javascript_control_code.find_one({ 'origin': control_url }) 
@@ -30,21 +20,42 @@ def save_control(db, filename, control_url):
    with open(filename, 'wb+') as fp:
        fp.write(script.get('code'))
 
-def save_script(db, filename, artefact_url, js_id=None):
+def lookup_artefacts(db, origin_url, xref):
+   """
+   One of origin_url and xref will not be None. If the URL then we must lookup an ETL hit to find
+   the control and artefact JS id to diff. If XREF then we obtain it from the db.vet_against_control collection.
+   The latter avoids an expensive lookup since it is by ID and obtains the correct timestamp artefacts so is 
+   recommended.
+   
+   Return result is always (control_url, origin_js_id, origin_url) or an Exception
+   """
+   if xref is None:
+       result = db.etl_hits.find_one({ 'origin_url': origin_url })
+       print(result)
+       if result is None:
+          raise ValueError("Failed to match {} - nothing to report!".format(origin_url))
+       return (result.get('control_url'), result.get('origin_js_id'), origin_url)
+   else:
+       for t in [db.vet_against_control, db.vet_against_control_20200517,  db.vet_against_control_20200524 ]:
+           result = t.find_one({ '_id': ObjectId(xref) })
+           if not result is None:
+               return (result.get('control_url'), result.get('origin_js_id'), result.get('origin_url'))
+       raise ValueError("Unable to locate xref {} - typo?".format(xref))
+
+
+def save_script(db, filename, js_id, artefact_url):
    assert db is not None
    assert isinstance(filename, str) and len(filename) > 0
+   assert js_id is not None and len(js_id) > 0
 
-   script, url_id = find_script(db, artefact_url)
+   script = db.scripts.find_one({ '_id': ObjectId(js_id) })
    if script is None:
-       print("Could not locate {} in database! Now try object id...".format(artefact_url))
-       script = db.scripts.find_one({ '_id': ObjectId(js_id) })
+       script, url_id = find_script(db, artefact_url)
        if script is None:
-           raise ValueError("Failed objectid lookup: {}!".format(js_id))
-       else:
-           print("Found {} in scripts collection".format(js_id))
-       # else FALLTHRU...
+            raise ValueError("Failed script lookup: {} {}!".format(js_id, artefact_url))
    else:
-       print("Found {} in scripts collection.".format(artefact_url))
+       print("Found {} in scripts collection".format(js_id))
+       # else FALLTHRU...
    with open(filename, 'wb+') as fp:
        fp.write(script.get('code'))
 
@@ -106,43 +117,50 @@ def report_vectors(db, artefact_fname, control_url: str, artefact_url: str):
        print("All literals seen the expected number of times.")
  
 
-# look for suitable control in etl_hits
-result = db.etl_hits.find_one({ 'origin_url': args.url })
-print(result)
-if not result:
-   print("Failed to find suitable control - nothing to report!")
-   exit(1)
+if __name__ == "__main__":
+   a = argparse.ArgumentParser(description="Run meld on the chosen URL as its best control, after JS beatification (optional)")
+   add_mongo_arguments(a, default_access='read-only', default_user='ro')
+   add_debug_arguments(a)
+   grp = a.add_mutually_exclusive_group(required=True)
+   grp.add_argument("--url", help="URL of Javascript to investigate (code fetched from DB, not internet)", type=str, default=None)
+   grp.add_argument("--xref", help="ID into db.vet_against_control collection (avoids URL -> ID lookup)", type=str, default=None)
+   a.add_argument("--diff", help="Diff program to run [/usr/bin/meld]", type=str, default="/usr/bin/meld")
+   a.add_argument("--beautifier", help="JS Beautifier to run [/usr/local/bin/js-beautify]", type=str, default="/usr/local/bin/js-beautify")
+   args = a.parse_args()
 
+   mongo = pymongo.MongoClient(args.db, args.port, username=args.dbuser, password=str(args.dbpassword))
+   db = mongo[args.dbname]
+   control_url, artefact_js_id, artefact_url = lookup_artefacts(db, args.url, args.xref)
+   assert control_url is not None and len(control_url) > 0
+   assert artefact_url is not None and len(artefact_url) > 0
+   assert artefact_js_id is not None and len(artefact_js_id) > 0
+   
+   with tempfile.TemporaryDirectory() as tdir: 
+      print(tdir)
 
-with tempfile.TemporaryDirectory() as tdir: 
-   print(tdir)
-   control_url = result.get('control_url')
-   if not len(control_url):
-       print("No suitable control {}".format(control_url))
+      ##### PREPARE TMP COPIES OF ARTEFACTS UNDER STUDY
+      # 1. prepare the artefacts into a temporary directory
+      control_fname = '{}/control.js'.format(tdir)
+      save_control(db, control_fname, control_url)
+      artefact_fname = '{}/artefact.js'.format(tdir)
+      save_script(db, artefact_fname, artefact_js_id, artefact_url)
 
-   ##### PREPARE TMP COPIES OF ARTEFACTS UNDER STUDY
-   # 1. prepare the artefacts into a temporary directory
-   control_fname = '{}/control.js'.format(tdir)
-   save_control(db, control_fname, control_url)
-   artefact_fname = '{}/artefact.js'.format(tdir)
-   save_script(db, artefact_fname, args.url, js_id=result.get('origin_js_id'))
+      #### REPORT VECTORS SUMMARISING THE NUMERIC DIFF
+      report_vectors(db, artefact_fname, control_url, artefact_url)
+	 
+      #### BEAUTIFY THEN DISPLAY GRAPHICAL DIFF
+      # 2. beautify them (ie. reduce minimisation to ensure consistent representation for diff'ing etc.)
+      bs1 = "{}/1c.js".format(tdir)
+      bs2 = "{}/2a.js".format(tdir)
+      proc = run([args.beautifier, "-o", bs1, "{}/control.js".format(tdir)])
+      if proc.returncode != 0:
+         raise Exception("Unable to beautify control!")
+      proc = run([args.beautifier, "-o", bs2, "{}/artefact.js".format(tdir)])
+      if proc.returncode != 0:
+         raise Exception("Unable to beautify artefact!")
 
-   #### REPORT VECTORS SUMMARISING THE NUMERIC DIFF
-   report_vectors(db, artefact_fname, control_url, args.url)
- 
-   #### BEAUTIFY THEN DISPLAY GRAPHICAL DIFF
-   # 2. beautify them (ie. reduce minimisation to ensure consistent representation for diff'ing etc.)
-   bs1 = "{}/1.js".format(tdir)
-   bs2 = "{}/2.js".format(tdir)
-   proc = run([args.beautifier, "-o", bs1, "{}/control.js".format(tdir)])
-   if proc.returncode != 0:
-      raise Exception("Unable to beautify control!")
-   proc = run([args.beautifier, "-o", bs2, "{}/artefact.js".format(tdir)])
-   if proc.returncode != 0:
-      raise Exception("Unable to beautify artefact!")
+      # 3. run meld on the resulting beautified-JS for both control and artefact
+      diff_proc = run([args.diff, bs1, bs2])
 
-   # 3. run meld on the resulting beautified-JS for both control and artefact
-   diff_proc = run([args.diff, bs1, bs2])
-
-mongo.close()
-exit(0)
+   mongo.close()
+   exit(0)
