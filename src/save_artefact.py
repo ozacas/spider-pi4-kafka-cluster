47e87d152264b348911b9c745df166fb489d21a6
Andrew Cassin <acassin999@gmail.com>
2020-06-06 11:54:02 +1000

    Updates to new APIs

--- src/save_artefact.py
+++ src/save_artefact.py
@@ -6,6 +6,7 @@ from bson.objectid import ObjectId
 from utils.misc import add_mongo_arguments
 from utils.models import JavascriptArtefact
 from utils.features import analyse_script
+from utils.io import get_script
 
 a = argparse.ArgumentParser(description="Save the specified artefact to disk as the specified filename")
 add_mongo_arguments(a, default_access="read-only", default_user='ro')
@@ -20,31 +21,32 @@ mongo = pymongo.MongoClient(args.db, args.port, username=args.dbuser, password=s
 db = mongo[args.dbname]
 
 if args.artefact:
-   ret = db.scripts.find_one({ '_id': ObjectId(args.artefact) })
+   code, js_id = get_script(args.artefact)
+   assert js_id == args.artefact
    if ret is None:
        raise ValueError("Unable to retrieve artefact {}".format(args.artefact))
 else: # args.control
    ret = db.javascript_control_code.find_one({ 'origin': args.control })
    if ret is None:
        raise ValueError("Cannot find control {}".format(args.control))
+   code = ret.get('code')
 
 with open(args.file, 'wb+') as fp:
    print("Saving artefact... {}".format(args.artefact))
-   fp.write(ret.get('code'))
+   fp.write(code)
 
 if args.artefact:
    jsr = JavascriptArtefact(url='foo', sha256='XXX', md5='YYY', inline=False) # doesnt matter from the perspective of dumping the literals
    byte_content, failed, stderr = analyse_script(args.file, jsr)
    if failed:
       raise ValueError("Unable to analyse script: {}\n{}".format(args.file, stderr))
-   ret = json.loads(byte_content)
+   vectors = json.loads(byte_content)
 else:
-   doc = db.javascript_control_code.find_one({'origin': args.control })
-   ret = json.loads(doc.get('analysis_bytes'))
+   vectors = json.loads(ret.get('analysis_bytes'))
 
-assert 'literals_by_count' in ret
+assert 'literals_by_count' in vectors
 if ret is not None and args.literals:
-   for k,v in ret.get('literals_by_count').items():
+   for k,v in vectors.get('literals_by_count').items():
        print(v, " ", k)
 
 mongo.close()
