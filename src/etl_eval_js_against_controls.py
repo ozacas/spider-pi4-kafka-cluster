d74e34f40530c7754c1c76ab3d241232ab2f56e0
Andrew Cassin <acassin999@gmail.com>
2020-06-12 21:17:40 +1000

    Huge changeover to new find_best_control() API. No longer need to load all controls into memory.

--- src/etl_eval_js_against_controls.py
+++ src/etl_eval_js_against_controls.py
@@ -8,9 +8,9 @@ import sys
 import hashlib
 import pylru
 from kafka import KafkaConsumer, KafkaProducer
-from utils.features import find_best_control, analyse_script, calculate_ast_vector, calculate_literal_distance, fix_literals, find_feasible_controls
+from utils.features import *
 from utils.models import JavascriptArtefact, JavascriptVectorSummary, BestControl
-from utils.io import next_artefact, load_controls
+from utils.io import next_artefact, load_controls, save_analysis_content
 from utils.misc import *
 from dataclasses import asdict
 
@@ -28,6 +28,10 @@ a.add_argument("--file", help="Debug specified file and exit []", type=str, defa
 a.add_argument("--min-size", help="Skip all controls less than X bytes [1500]", type=int, default=1500)
 a.add_argument("--defensive", help="Validate each message from kafka for corruption (slow)", action="store_true")
 a.add_argument("--max-distance", help="Limit control hits to those with AST*Call distance < [150.0]", type=float, default=150.0)
+g = a.add_mutually_exclusive_group(required=False)
+g.add_argument("--report-bad", help="Report artefacts to stdout which dont hit anything [False]", action="store_true")
+g.add_argument("--report-good", help="Report artefacts to stdout which hit a control [False]", action="store_true")
+g.add_argument("--report-all", help="Report all artefacts to stdout regardless of distance [False]", action="store_true")
 args = a.parse_args()
 
 mongo = pymongo.MongoClient(args.db, args.port, username=args.dbuser, password=str(args.dbpassword))
@@ -46,33 +50,25 @@ def cleanup(*args):
     rm_pidfile('pid.eval.controls')
     sys.exit(0)
 
-
-# 0. read controls once only (TODO FIXME: assumption is that the vectors fit entirely in memory)
-all_controls = []
-for t in load_controls(db, all_vectors=True, min_size=args.min_size, verbose=args.v):
-   assert isinstance(t, tuple)
-   rec, ast_sum, ast_vector, call_vector, _ = t
-   assert isinstance(rec, dict)
-   all_controls.append((rec, ast_sum, ast_vector, call_vector ))
-
-print("Loaded {} javascript controls.".format(len(all_controls)))
-
 if args.v:
-   print("Reporting unique families with JS controls (please wait this may take some time):")
    print(db.javascript_controls.distinct('family'))
 
 if args.file:
    with open(args.file, 'rb') as fp:
        content = fp.read()
        jsr = JavascriptArtefact(url=args.file, sha256=hashlib.sha256(content).hexdigest(), 
-                                md5=hashlib.md5(content).hexdigest(), size_bytes=len(content))
+                                md5=hashlib.md5(content).hexdigest(), size_bytes=len(content), js_id='0' * 24) # specify fake js_id to avoid assert failure
        byte_content, failed, stderr = analyse_script(content, jsr, feature_extractor=args.extractor)
        if failed:
-           print("Unable to extract features... aborting.")
-           print(stderr)
+           print("Unable to extract features... aborting.\n{}".format(stderr))
            cleanup()
-       best_control, next_best_control = find_best_control(json.loads(byte_content.decode()), all_controls,
-                                                           db=db, max_distance=args.max_distance, debug=True) 
+       m = json.loads(byte_content.decode())
+       m['js_id'] = jsr.js_id
+       m['origin'] = jsr.url
+       m['url'] = jsr.url
+       m['byte_content_sha256'] = hashlib.sha256(byte_content).hexdigest()
+       best_control, next_best_control = find_best_control(db, m, 
+                                                           max_distance=args.max_distance, debug=True) 
        print("*** WINNING CONTROL HIT")
        print(best_control)
        print("*** NEXT BEST CONTROL HIT")
@@ -118,6 +114,40 @@ def get_control_bytes(db, control_url: str):
     assert hashlib.sha256(doc['analysis_bytes']).hexdigest() == doc['analysis_vectors_sha256']
     return doc['analysis_bytes']
 
+def find_or_update_analysis_content(db, m, fail_iff_not_found=False, defensive=False):
+    assert isinstance(m, dict) 
+
+    js_id = m.get('js_id')
+    assert len(js_id) > 0
+
+    # NB: due to an error in processing, I had to throw away the db.analysis_content collection, so records may be missing. Sigh 8th June 2020
+    byte_content_doc = db.analysis_content.find_one({ 'js_id': js_id })
+    if fail_iff_not_found and byte_content_doc is None:   # prevent infinite recursion
+       raise ValueError("No data for {}".format(js_id))
+
+    if byte_content_doc is None:
+        print("WARNING: analysis data could not be found: {} - recalculating...".format(m))
+        code_bytes, js_id = get_script(db, js_id)
+        assert code_bytes is not None
+        jsr = JavascriptArtefact(url=m.get('url'), sha256=m.get('sha256'), md5=m.get('md5'), size_bytes=m.get('size_bytes'), js_id=js_id)
+        vector_as_bytes, failed, stderr = analyse_script(code_bytes, jsr, java=args.java, feature_extractor=args.extractor)
+        if failed:
+            raise ValueError("Could not analyse artefact: js_id={}\n{}".format(js_id, stderr))
+        expected_hash = save_analysis_content(db, jsr, vector_as_bytes)
+
+        if defensive:
+            # check the byte content hashes matched
+            if m.get('byte_content_sha256') != expected_hash: 
+                print("***** WARNING: hashes didnt match for JS id: {}".format(js_id))
+            # but also that the artefact hashes match the actual content 
+            assert hashlib.sha256(code_bytes).hexdigest() == m.get('sha256')
+        return find_or_update_analysis_content(db, m, fail_iff_not_found=True) # this time it should be found!
+
+    assert 'analysis_bytes' in byte_content_doc
+    byte_content = byte_content_doc.get('analysis_bytes')
+    assert isinstance(byte_content, bytes)
+    return (byte_content, byte_content_doc.get('byte_content_sha256'))
+ 
 def update_literal_distance(db, hit: BestControl, ovec, fail_if_difference=False):
     assert hit is not None
     assert ovec is not None
@@ -129,7 +159,9 @@ def update_literal_distance(db, hit: BestControl, ovec, fail_if_difference=False
          hit.n_diff_literals = -1
          hit.diff_literals = ''
          return
-    t = calculate_literal_distance(json.loads(cvec).get('literals_by_count'), ovec, fail_if_difference=fail_if_difference)
+    v1 = truncate_literals(json.loads(cvec).get('literals_by_count'))
+    v2 = truncate_literals(ovec)
+    t = calculate_literal_distance(v1, v2, fail_if_difference=fail_if_difference)
     hit.literal_dist, hit.literals_not_in_origin, hit.literals_not_in_control, diff_literals = t
     hit.n_diff_literals = len(diff_literals)
     hit.diff_literals = fix_literals(diff_literals) 
@@ -140,33 +172,26 @@ print("Creating required index in vet_against_control collection... please wait"
 db.vet_against_control.create_index([( 'origin_url', pymongo.ASCENDING), ('control_url', pymongo.ASCENDING )], unique=True)
 print("Index creation complete.")
 
+vector_cache = pylru.lrucache(10 * 1000) # bigger the better since we want to avoid large-scale mongo queries
 for m in next_artefact(consumer, args.n, filter_cb=lambda m: m.get('size_bytes') >= 1500, verbose=args.v):
     js_id = m.get('js_id')
     if not 'byte_content_sha256' in m:
         continue
-    required_hash = m.get('byte_content_sha256')
-    byte_content_doc = db.analysis_content.find_one({ 'js_id': js_id, 'byte_content_sha256': required_hash })
-    if byte_content_doc is None:
-        print("WARNING: analysis data could not be found: {}".format(m))
-        continue
-    assert 'analysis_bytes' in byte_content_doc
-    byte_content = byte_content_doc.get('analysis_bytes')
-    assert isinstance(byte_content, bytes)
-    if args.defensive:
-       # 1. check byte vector hashes match
-       actual_sha256 =  hashlib.sha256(byte_content).hexdigest() 
-       if args.v:
-           print("byte_vectors: ", actual_sha256, " ", required_hash, " origin js_id:", js_id)
-       assert actual_sha256 == required_hash
-    d = json.loads(byte_content)
+    byte_content, required_hash = find_or_update_analysis_content(db, m)
+    assert byte_content is not None
+    assert required_hash is not None
+    d = json.loads(byte_content.decode())
     for t in ['statements_by_count', 'calls_by_count', 'literals_by_count']:
         m[t] = d[t]
+
     # example m: {'url': 'https://homes.mirvac.com/-/media/Project/Mirvac/Residential/Base-Residential-Site/Base-Residential-Site-Theme/scripts/navigationmin.js', 
     #             'sha256': 'd6941fcfdd12069e20f4bb880ecbab12d797d9696cae1b05ec9d59fb9bd90b51', 'md5': '2b10377115ab0747535acb1ad38b26bd', 
     #             'inline': False, 'content_type': 'text/javascript', 'when': '2020-06-04 03:28:28.483855', 'size_bytes': 14951, 
     #             'origin': 'https://homes.mirvac.com/homes-portfolio',  'js_id': '5e8ef7df582045cdd24ce8ae', 
     #             'byte_content_sha256': 'b2b45feee3497bee1e575eb56c50a84ec7651dbc160e8b1607b07153563d804c'}
-    best_control, next_best_control = find_best_control(m, all_controls, db=db, control_cache=pylru.lrucache(200), max_distance=args.max_distance)
+    best_control, next_best_control = find_best_control(db, m, debug=args.v,
+                                                        control_cache=vector_cache,
+                                                        max_distance=args.max_distance)
     ovec = m['literals_by_count']
     if best_control is None or len(best_control.control_url) == 0:
         pass 
@@ -181,14 +206,16 @@ for m in next_artefact(consumer, args.n, filter_cb=lambda m: m.get('size_bytes')
             assert cntl_doc.get('sha256') == m['sha256']  # these exist to catch sha256 hash collisions if any, to verify whether if they happen in production...
             assert cntl_doc.get('md5') == m['md5']
             assert cntl_doc.get('size_bytes') == m['size_bytes']
-        #update_literal_distance(db, best_control, ovec, fail_if_difference=args.defensive and best_control.sha256_matched)
-        update_literal_distance(db, best_control, ovec, fail_if_difference=False)
+        update_literal_distance(db, best_control, ovec, fail_if_difference=args.defensive and best_control.sha256_matched)
+        #update_literal_distance(db, best_control, ovec, fail_if_difference=False)
     d = save_vetting(db, best_control, required_hash)
     best_control.xref = d['xref']
     assert best_control.xref is not None
-
     producer.send(args.to, d) 
-    if args.v and len(best_control.control_url) > 0:  # only report hits in verbose mode, to make for easier investigation
+
+    if (args.report_bad or args.report_all) and len(best_control.control_url) == 0:
+        print(best_control)
+    if (args.report_good or args.v or args.report_all) and len(best_control.control_url) > 0:
         print(best_control)
 
     if next_best_control is not None and len(next_best_control.control_url) > 0:
